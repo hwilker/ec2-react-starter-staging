00d93b38a8509eb495af3ce8c94c5698c939d53d
hwilker <harry@hwilker.com>
2024-09-15 17:33:53 -0700

    edit deploy.yml to trigger on stageing.

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/api/hello', (req, res) => {
-  res.json({msg: 'Hello staging World!!'})
+  res.json({msg: 'Hello staging server World!!'})
 })
 
 app.get('/api/goodbye', (req, res) => {
